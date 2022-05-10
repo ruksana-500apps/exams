a54166c763f83910af0ee57f1c09efa50d0ca2f2
Ruksana-500 <ruksana.shaik@500apps.com>
2022-05-10 19:26:32 +0530

    Exam

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <products></products>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import products from '../src/components/products.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   products
   }
 }
 </script>
---
+++ src/components/products.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-form-input  v-model="product_name" placeholder="Add product name" required></b-form-input>
+    <b-form-input  v-model="product_price" placeholder="Add product price" required></b-form-input>
+    <b-form-input  v-model="product_category" placeholder="Add product category" required></b-form-input>
+    <b-button @click=add() >Add</b-button>
+    <b-table  striped hover :items="products" :fields="fields">
+    <!--  <listview :items="products"></listview>-->
+    </b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+      name:"producT",
+    data() {
+      return {
+        products: [
+          { name: "Mobiles", price: 20000, category: 'electronics' },
+           { name: "Laptops", price: 50000, category: 'electronics' },
+            { name: "Pens", price: 20, category: 'stationary' },
+             { name: "pencils", price: 10, category: 'stationary' },
+        ],
+        fields:["name","price","category"],
+        product_name:'',
+        product_price:'',
+        product_category:''
+       
+      }
+    },
+    methods:{
+        add(){
+          this.products.push({name:this.product_name, price:this.product_price  ,category:this.product_category})
+
+         
+        }
+    }
+  }
+</script>
